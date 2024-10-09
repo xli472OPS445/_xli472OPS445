945ee96123211a4dd2ad1dde09a1bfadd20676db
XIN LI <xli472@myseneca.ca>
2024-10-09 10:21:25 -0400

    add lab4c

---
+++ lab4c.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Dictionaries
+dict_york = {'Address': '70 The Pond Rd', 'City': 'Toronto', 'Country': 'Canada', 'Postal Code': 'M3J3M6', 'Province': 'ON'}
+dict_newnham = {'Address': '1750 Finch Ave E', 'City': 'Toronto', 'Country': 'Canada', 'Postal Code': 'M2J2X5', 'Province': 'ON'}
+# Lists
+list_keys = ['Address', 'City', 'Country', 'Postal Code', 'Province']
+list_values = ['70 The Pond Rd', 'Toronto', 'Canada', 'M3J3M6', 'ON']
+
+def create_dictionary(keys, values):
+    result = {}  
+    i = 0
+    while i < len(keys):
+        result[keys[i]] = values[i]  
+        i += 1
+    return result 
+
+def shared_values(dict1, dict2):
+    new_dic = set(dict1.values()) & set(dict2.values())
+    return new_dic
+
+if __name__ == '__main__':
+    york = create_dictionary(list_keys, list_values)
+    print('York: ', york)
+    common = shared_values(dict_york, dict_newnham)
+    print('Shared Values', common)
