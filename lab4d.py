3a3cca31209560dd149f6b82282156233ef33700
XIN LI <xli472@myseneca.ca>
2024-10-09 14:34:25 -0400

    add lab4d.py

---
+++ lab4d.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# Strings 1
+
+str1 = 'Hello World!!'
+str2 = 'Seneca College'
+
+num1 = 1500
+num2 = 1.50
+
+def first_five(s):
+    return s[0:5]
+
+def last_seven(s):
+    return s[-7:]
+
+def middle_number(s):
+    return str(s)[1:3]
+
+def first_three_last_three(f, l):
+    return f[0:3] + l[-3:]
+
+if __name__ == '__main__':
+    print(first_five(str1))
+    print(first_five(str2))
+    print(last_seven(str1))
+    print(last_seven(str2))
+    print(middle_number(num1))
+    print(middle_number(num2))
+    print(first_three_last_three(str1, str2))
+    print(first_three_last_three(str2, str1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
